ea864a140ebfe1479ab4bf79f1f84ff8a484f83f
Christopher Bare <christopherbare@outlook.com>
2019-05-30 08:27:16 -0400

    Corrected some things.

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,7 @@ public class Main {
         int discs = scanner.nextInt();
         solve(discs, "A", "B", "C");
     }
-    /*
-    Step 1: Move (n-1) discs from pole1 to pole2
-    Step 2: Move the nth disc (last disc) from pole1 to pole3.
-    Step 3: Now move the n-1 discs which is present in pole2 to pole3.
-    Step1 and Step3 will be recursive.
-     */
+
     public static void solve(int numRings, String start, String auxiliary, String end) {
         // Check if rings = 1, print out moving the ring from start -> end
         if (numRings == 1) {
@@ -28,3 +23,4 @@ public class Main {
         }
     }
 }
+
